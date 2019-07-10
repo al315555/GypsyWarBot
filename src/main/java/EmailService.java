1acf81c75366869ce2b5ef587e773b06fcbe1e09
RHigueras <31961262+al315555@users.noreply.github.com>
2019-07-10 17:12:41 +0200

    Email configuration & init of BattlesAlgorithm

---
+++ src/main/java/EmailService.java
@@ -0,0 +1,56 @@
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class EmailService {
+    private final Properties properties = new Properties();
+    private String password;
+    private boolean init;
+    private Session session;
+
+    public void init() throws Exception {
+        try {
+            Properties props = new Properties();
+            InputStream is = EmailService.class.getClassLoader().getResourceAsStream("email.properties");
+            props.load(is);
+            is.close();
+            properties.put("mail.smtp.host", props.getProperty("host"));
+            properties.put("mail.smtp.starttls.enable", props.getProperty("starttls"));
+            properties.put("mail.smtp.port", props.getProperty("port"));
+            properties.put("mail.smtp.mail.sender", props.getProperty("mail"));
+            properties.put("mail.smtp.user", props.getProperty("user"));
+            properties.put("mail.smtp.auth", props.getProperty("auth"));
+            properties.put("mail.smtp.password", props.getProperty("password"));
+            password = props.getProperty("password");
+
+            session = Session.getDefaultInstance(properties);
+            session.setDebug(true);
+            init = true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new Exception(ex);
+        }
+
+    }
+
+    public void sendEmail() throws Exception {
+        if (!init) return;
+
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress((String) properties.get("mail.smtp.mail.sender")));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress("al315555@uji.es"));
+        message.setSubject("Prueba");
+        message.setText("Texto");
+        Transport t = session.getTransport("smtp");
+        t.connect((String) properties.get("mail.smtp.user"), password);
+        t.sendMessage(message, message.getAllRecipients());
+        t.close();
+
+
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,27 @@ import static spark.Spark.*;
 public class Main {
     public static final int PORT_BY_DEFAULT = 4567;
     public static final String PORT = "PORT";
+    private static EmailService emailService = new EmailService();
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         get("/", (req, res) -> "Main Screen: Gypsy family bot. Created by Ruben H.");
         get("/about", (req, res) -> "Gypsy family bot. Created by Ruben H.");
+        get("/sendEmailTest", (req, res) -> {
+            emailService.sendEmail();
+            return "Gypsy family bot. Created by Ruben H.";
+        });
     }
     static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get(PORT) != null) {
-            return Integer.parseInt(processBuilder.environment().get(PORT));
+        try {
+            emailService.init();
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            if (processBuilder.environment().get(PORT) != null) {
+                return Integer.parseInt(processBuilder.environment().get(PORT));
+            }
+        }catch (Exception exception) {
+            exception.printStackTrace();
+        }finally {
+            return PORT_BY_DEFAULT; //return default port if heroku-port isn't set (i.e. on localhost)
         }
-        return PORT_BY_DEFAULT; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
